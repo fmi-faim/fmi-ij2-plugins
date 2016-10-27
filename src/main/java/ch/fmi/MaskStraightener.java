4a52455a9a1e5e8772ba9b4b12a2bbcc0508d347
Jan Eglinger <jan.eglinger@fmi.ch>
2016-10-27 15:07:02 +0200

    Add initial version of MaskStraightener

---
+++ src/main/java/ch/fmi/MaskStraightener.java
@@ -0,0 +1,73 @@
+package ch.fmi;
+
+import java.awt.Polygon;
+import java.util.ArrayList;
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.gui.PolygonRoi;
+import ij.gui.Roi;
+import ij.plugin.Straightener;
+import ij.process.ByteProcessor;
+import ij.process.ImageProcessor;
+
+import org.scijava.ItemIO;
+import org.scijava.command.Command;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+import sc.fiji.analyzeSkeleton.AnalyzeSkeleton_;
+import sc.fiji.analyzeSkeleton.Point;
+
+@Plugin(type = Command.class, headless = true, menuPath = "FMI>Straighten Image")
+public class MaskStraightener implements Command {
+	@Parameter(label="Input image")
+	private ImagePlus imp;
+	
+	@Parameter(label="Object mask")
+	private ImagePlus mask;
+	
+	@Parameter(label="Line Width")
+	private int lineWidth;
+
+	@Parameter(type = ItemIO.OUTPUT)
+	private ImagePlus result;
+	
+	@Override
+	public void run() {
+		//IJ.run(mask, "8-bit", ""); // to make sure the image is 8-bit grayscale
+		//IJ.run(mask, "Skeletonize (2D/3D)", "");
+		ByteProcessor maskIp = mask.getProcessor().convertToByteProcessor(true);
+		maskIp.invert(); // work around IJ1 processors working with white background
+		maskIp.skeletonize();
+		maskIp.invert(); // work around IJ1 processors working with white background
+
+		// Initialize AnalyzeSkeleton_
+		AnalyzeSkeleton_ skel = new AnalyzeSkeleton_();
+		AnalyzeSkeleton_.calculateShortestPath = true;
+		skel.setup("", new ImagePlus("Skeleton", maskIp));
+		 
+		// Perform analysis in silent mode
+		skel.run(AnalyzeSkeleton_.NONE, false, true, null, true, false);
+
+		// Read the results
+		ArrayList<Point>[] sppoints = skel.getShortestPathPoints();
+
+		// Create polyline ROI by averaging pairs of points
+		Polygon poly = new Polygon();
+		for (int i = 0; i<sppoints[0].size()-1; i+=2) {
+			int x = (sppoints[0].get(i).x + sppoints[0].get(i+1).x)/2;
+			int y = (sppoints[0].get(i).y + sppoints[0].get(i+1).y)/2;
+			poly.addPoint(x,y);
+		}
+		PolygonRoi pRoi = new PolygonRoi(poly, Roi.POLYLINE);
+
+		// Create straightened image for kymograph etc.
+		imp.setRoi(pRoi);
+		//IJ.run(imp, "Straighten...", "line=20");
+		Straightener st = new Straightener();
+		ImageProcessor ip = st.straightenLine(imp, lineWidth);
+
+		result = new ImagePlus("Straightened", ip);
+	}
+}
