5c273560ebedac9cd4f196957d8cdf7e75ed8c9a
Jan Eglinger <jan.eglinger@fmi.ch>
2017-02-22 15:00:50 +0100

    Functional EllipseFitter

--- src/main/java/ch/fmi/EllipseFitter.java
+++ src/main/java/ch/fmi/EllipseFitter.java
@@ -1,42 +1,60 @@
 package ch.fmi;
 
-import ij.ImagePlus;
+import java.util.ArrayList;
 
+import net.imglib2.Cursor;
+import net.imglib2.img.Img;
+import net.imglib2.type.numeric.IntegerType;
+
+import org.doube.geometry.FitEllipse;
 import org.scijava.ItemIO;
 import org.scijava.command.Command;
+import org.scijava.log.LogService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
 @Plugin(type = Command.class, headless = true, menuPath = "FMI>Center of Ellipse Fit")
-public class EllipseFitter implements Command {
-	@Parameter(label="Input image")
-	private ImagePlus imp;
+public class EllipseFitter<T extends IntegerType<T>> implements Command {
+	@Parameter
+	private LogService logService;
 
-	@Parameter(type=ItemIO.OUTPUT)
+	@Parameter(label = "Input image")
+	private Img<T> img;
+
+	@Parameter(label = "Background value")
+	private int bg = 0;
+
+	@Parameter(type = ItemIO.OUTPUT)
 	private double centerX;
-	
-	@Parameter(type=ItemIO.OUTPUT)
-	private double centerY;
 
-	@Parameter(type=ItemIO.OUTPUT)
-	private double[] testOut;
+	@Parameter(type = ItemIO.OUTPUT)
+	private double centerY;
 
 	@Override
 	public void run() {
 		// Get points from binary mask (each white pixel will get added)
-		// TODO Get Cursor from Img<T>, for all pixels != background: add point
+		ArrayList<double[]> polygonPoints = new ArrayList<>();
+		Cursor<T> cursor = img.localizingCursor();
+		while (cursor.hasNext()) {
+			if (cursor.next().getInteger() > bg) {
+				double[] pos = new double[2];
+				pos[0] = cursor.getDoublePosition(0);
+				pos[1] = cursor.getDoublePosition(1);
+				polygonPoints.add(pos);
+			}
+		}
+
+		double[][] points = new double[polygonPoints.size()][];
+		points = polygonPoints.toArray(points);
 		
-		// Fit ellipse to point cloud, using Michael Doube's EllipseFit
+		logService.info("Ellipse fitting: found " + points.length + " points.");
 		
+		// Fit ellipse to point cloud, using Michael Doube's FitEllipse
+		double[] ellipseParams = FitEllipse.direct(points);
+		double[] ellipseDims = FitEllipse.varToDimensions(ellipseParams);
+
 		// Get center coordinates
-		
-		
-		// DEBUG Return fake coordinates
-		centerX=imp.getWidth()/2.0;
-		centerY=imp.getHeight()/2.0;
-		
-		testOut = new double[2];
-		testOut[0] = 0.1;
-		testOut[1] = 1.345;
+		centerX = ellipseDims[0];
+		centerY = ellipseDims[1];
 	}
 }
