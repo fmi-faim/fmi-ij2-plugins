663406b15c41edd7765e6f2571fd9c98f0eecd48
Jan Eglinger <jan.eglinger@fmi.ch>
2021-02-26 09:33:35 +0100

    Update license headers

--- src/main/java/org/doube/geometry/Centroid.java
+++ src/main/java/org/doube/geometry/Centroid.java
@@ -2,7 +2,7 @@
  * #%L
  * A collection of plugins developed at the FMI Basel.
  * %%
- * Copyright (C) 2016 - 2020 FMI Basel
+ * Copyright (C) 2016 - 2021 FMI Basel
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
